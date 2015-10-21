7731aa5b9444bf2604c665965fd06f585f445b3a
Joel <joelrlneto@gmail.com>
2015-10-21 16:16:42 -0300

    Integração da tela de detalhes da tarefa (closes #8)

--- SigaaWindows/SigaaWindows/View/TaskDetails.xaml.cs
+++ SigaaWindows/SigaaWindows/View/TaskDetails.xaml.cs
@@ -20,16 +20,11 @@ namespace SigaaWindows.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Home : Page
+    public sealed partial class TaskDetails : Page
     {
-        public Home()
+        public TaskDetails()
         {
             this.InitializeComponent();
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(TaskDetails));
         }
     }
 }
